c2ccfd11ffbf25b521f029289085795a43e01277
q2727 <91459529+q2727@users.noreply.github.com>
2023-03-31 11:49:14 +0800

    Delete shelllab(done) directory

--- shelllab(done)/cmake-build-debug/CMakeFiles/shelllab.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for shelllab.
-# This may be replaced when dependencies are built.
--- shelllab(done)/foo/foo1.c
+++
@@ -1,7 +0,0 @@
-#include "stdio.h"
-#include <unistd.h>
-int main(int argc,char **argv){
-    sleep(1000);
-    printf("foo1 out\n");
-    return 0;
-}
