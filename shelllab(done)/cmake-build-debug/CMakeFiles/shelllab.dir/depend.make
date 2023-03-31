73dbd77bbd34cc110520cf1204e4000bbaa11e03
qc2727 <284432065@qq.com>
2023-03-31 11:20:15 +0800

    Add shelllab file

---
+++ shelllab(done)/cmake-build-debug/CMakeFiles/shelllab.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for shelllab.
+# This may be replaced when dependencies are built.
---
+++ shelllab(done)/foo/foo1.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+#include <unistd.h>
+int main(int argc,char **argv){
+    sleep(1000);
+    printf("foo1 out\n");
+    return 0;
+}
